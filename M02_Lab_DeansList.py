ed9e8e6b7323c9e7e9935e2e3441ec3dd143e9a6
Anthony Harris <aharris@nyb.com>
2023-06-16 14:52:43 -0500

    first commit

---
+++ M02_Lab_DeansList.py
@@ -0,0 +1,24 @@
+##  Name: Anthony Harris
+##  File Name: M02_Lab_DeansList.py
+##  This program asks XXX
+
+lastNameInputText = ("Please enter the last name of student {}: ")
+firstNameInputText = ("Please enter the first name of student {}: ")
+counter = 1
+gpa = 0.00
+studentList = []
+gpaList = []
+
+print("Student GPA Tracker")
+print()
+lastName = (input(lastNameInputText.format(counter)))
+
+while lastName != "ZZZ":
+    firstName = (input(firstNameInputText.format(counter)))
+    fullName = (firstName.strip()).capitalize() + " " + (lastName.strip()).capitalize()
+    
+    gpa = int((input("What is {}'s GPA?: ".format(fullName))(fullName)))
+
+    lastName = (input(lastNameInputText.format(counter + 1)))
+
+print("End Program")
